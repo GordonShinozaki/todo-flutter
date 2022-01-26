664c155f15e3e0f2a812c6d14b518b59fd14d615
dekai.wah <dekai.wah@linecorp.com>
2022-01-26 23:27:22 +0900

    feat: refactor Todo Card

--- lib/main.dart
+++ lib/main.dart
@@ -26,25 +26,42 @@ class MyHomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: Text("Menu Bar"),
+      appBar: AppBar(
+        title: Text("Menu Bar"),
+      ),
+      body: Center(
+        child: ListView(children: [
+          // ここから下が修正箇所
+          TodoCardWidget(label: "TODO 1"),
+          TodoCardWidget(label: "TODO 2"),
+          TodoCardWidget(label: "TODO 3"),
+          TodoCardWidget(label: "TODO 4"),
+        ]),
+      ),
+    );
+  }
+}
+
+////////////////////
+// ④ TodoCardのWidget
+class TodoCardWidget extends StatelessWidget {
+  final String label;
+
+  TodoCardWidget({Key? key, required this.label});
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      margin: EdgeInsets.all(10),
+      child: Container(
+        padding: EdgeInsets.all(10),
+        child: Row(
+          children: [
+            Checkbox(onChanged: null, value: false),
+            Text(this.label),
+          ],
         ),
-        body: Center(
-          child: ListView(
-            children: [Card(
-              margin: EdgeInsets.all(10),
-              child: Container(
-                padding: EdgeInsets.all(10),
-                child: Row(
-                  children:[
-                    Checkbox(onChanged: null, value: false),
-                    Text("TODO 1"),
-                  ],   
-                ),
-              ),
-            )],
-        ),
-        ),
+      ),
     );
   }
 }
